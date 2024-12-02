973833c2aa4f8356f0a6822fd1969428137d5875
bobbymannino <manninobobby@icloud.com>
2024-12-02 21:49:16 +0000

    feat: upgrade to svelte 5

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,9 +2,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bulma/css/bulma.min.css'
 import App from './App.svelte'
 import './overrides.css'
+import { mount } from 'svelte'
 
-const app = new App({
-  target: document.getElementById('app')
-})
+const app = mount(App, { target: document.getElementById('app') })
 
 export default app
