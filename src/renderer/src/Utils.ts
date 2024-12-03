a5d931a3f7236d4423ec5e580dd0f8d63f04c399
bobbymannino <manninobobby@icloud.com>
2024-12-03 12:21:18 +0000

    feat: allow spaces in username (exc char 0)

--- src/renderer/src/Utils.ts
+++ src/renderer/src/Utils.ts
@@ -23,7 +23,9 @@ export const checkIsValidHexColor = (color: string): boolean => {
 }
 
 export const checkIsUsernameValid = (username: string): boolean => {
-  return /^[a-zA-Z0-9_]{1,31}$/.test(username) // 1 to 31 inclusive
+  // 1-31 characters inc.
+  // cannot start with a space
+  return /^[a-zA-Z0-9_][a-zA-Z0-9_\s]{0,30}$/.test(username)
 }
 
 export const externalLinkClickHandler = (root: HTMLButtonElement, url: string): void => {
