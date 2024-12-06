f0c51f1f59dcfad08729b19d7c79c206183f40d4
Marco Kellershoff <marco@kellershoff.net>
2024-12-07 00:13:05 +0100

    fix(utils): ts error in debounce

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -18,7 +18,7 @@ export const debounce = <T extends (...args: unknown[]) => void>(
   func: T,
   wait: number
 ): ((...args: Parameters<T>) => void) => {
-  let timeout: NodeJS.Timeout
+  let timeout: ReturnType<typeof setTimeout>
   return (...args: Parameters<T>): void => {
     clearTimeout(timeout)
     timeout = setTimeout(() => {
