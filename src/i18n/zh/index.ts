845e2c2161a4b16b1bc94726eaabbb93866993dc
Marco Kellershoff <marco@kellershoff.net>
2025-01-14 18:29:05 +0100

    feat(node): add typesafe-i18n package

---
+++ src/i18n/zh/index.ts
@@ -0,0 +1,39 @@
+import type { Translation } from '../i18n-types'
+
+const zh = {
+  about: 'About',
+  advanced: 'Advanced',
+  basic: 'Basic',
+  cancel: 'Cancel',
+  choose_a_color: 'Choose a color',
+  color: 'Color',
+  connect: 'Connect',
+  connection_established: 'Connection established',
+  copy_my_connection_string: 'Copy my connection string',
+  disconnect: 'Disconnect',
+  fullscreen: 'Fullscreen',
+  host_a_session: 'Host a session',
+  host_connection_string: 'Host connection string',
+  hosting_a_session: 'Hosting a session',
+  is_microphone_active_on_connect: 'Is microphone active on connect',
+  join_a_session: 'Join a session',
+  joined_a_session: 'Joined a session',
+  media: 'Media',
+  microphone_active: 'Microphone active',
+  microphone_inactive: 'Microphone inactive',
+  not_streaming_your_display: 'Not streaming your display',
+  participant_connection_string: 'Participant connection string',
+  remote_cursors_disabled: 'Remote cursors disabled',
+  remote_cursors_enabled: 'Remote cursors enabled',
+  save: 'Save',
+  session_started: 'Session started',
+  settings: 'Settings',
+  start_a_new_session: 'Start a new session',
+  streaming_your_display: 'Streaming your display',
+  stun_turn_server_objects: 'STUN/TURN Server Objects (separated by new lines)',
+  username: 'Username',
+  zoom_in: 'Zoom in',
+  zoom_out: 'Zoom out'
+} satisfies Translation
+
+export default zh
